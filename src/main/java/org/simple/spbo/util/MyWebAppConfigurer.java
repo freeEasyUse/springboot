732b76d4d2ce5e64521608b7f4a26f22e5e86f4a
yekailoveqq <yekailoveqq@163.com>
2016-09-01 13:48:59 +0800

    自定义静态文件的映射路径

---
+++ src/main/java/org/simple/spbo/util/MyWebAppConfigurer.java
@@ -0,0 +1,14 @@
+package org.simple.spbo.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/**").addResourceLocations(
+				"classpath:/static/img/");
+		super.addResourceHandlers(registry);
+	}
+}
