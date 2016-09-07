6abffd4b2b02d5090528f0bc272b9e4286b43ebc
yekailoveqq <yekailoveqq@163.com>
2016-09-07 14:02:39 +0800

    spring boot 支持热部署

--- src/main/java/org/simple/spbo/shiro/ShiroConfiguration.java
+++ src/main/java/org/simple/spbo/shiro/ShiroConfiguration.java
@@ -7,7 +7,9 @@ import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.simple.spbo.shiro.realms.MyShiroRealm;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -100,7 +102,7 @@ public class ShiroConfiguration {
        //<!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
         //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
        filterChainDefinitionMap.put("/shiro/**", "authc");
-      
+       
        // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
         shiroFilterFactoryBean.setLoginUrl("/shiro/login");
         // 登录成功后要跳转的链接
@@ -128,6 +130,33 @@ public class ShiroConfiguration {
        return cacheManager;
     }
    
+    /**
+     * cookie对象;
+     * @return
+     */
+    @Bean
+    public SimpleCookie rememberMeCookie(){
+       System.out.println("ShiroConfiguration.rememberMeCookie()");
+       //这个参数是cookie的名称，对应前端的checkbox的name = rememberMe
+       SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
+       //<!-- 记住我cookie生效时间30天 ,单位秒;-->
+       simpleCookie.setMaxAge(259200);
+       return simpleCookie;
+    }
+   
+    /**
+     * cookie管理对象;
+     * @return
+     */
+    @Bean
+    public CookieRememberMeManager rememberMeManager(){
+       System.out.println("ShiroConfiguration.rememberMeManager()");
+       CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+       cookieRememberMeManager.setCookie(rememberMeCookie());
+       return cookieRememberMeManager;
+    }
+    
+    
     @Bean
     public SecurityManager securityManager(){
        DefaultWebSecurityManager securityManager =  new DefaultWebSecurityManager();
@@ -135,6 +164,9 @@ public class ShiroConfiguration {
        securityManager.setRealm(myShiroRealm());
        //注入缓存管理器;
        securityManager.setCacheManager(ehCacheManager());//这个如果执行多次，也是同样的一个对象;
+       
+       //注入记住我管理器;
+       securityManager.setRememberMeManager(rememberMeManager());
        return securityManager;
     }
    
